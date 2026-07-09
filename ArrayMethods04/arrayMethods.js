ca0a48303f08700325a591a6210d181be575d3fc
Rajatsainiji <rajatsaini798317@gmail.com>
2026-07-09 21:47:18 +0530

    array methods question

---
+++ ArrayMethods04/arrayMethods.js
@@ -0,0 +1,13 @@
+// Task 1
+// const fruits = ["Apple", "Banana"];
+// Add:
+// "Mango"
+// Expected:
+// ["Apple", "Banana", "Mango"]
+
+
+const fruits = ["Apple", "Banana"];
+
+fruits.push("Mango");
+
+console.log(fruits);
