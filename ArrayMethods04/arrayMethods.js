7d4612aad0aca6ccb95a2fb5144bb9eda2c6bb3b
Rajatsainiji <rajatsaini798317@gmail.com>
2026-07-10 22:17:07 +0530

    array methods question

--- ArrayMethods04/arrayMethods.js
+++ ArrayMethods04/arrayMethods.js
@@ -11,3 +11,23 @@ const fruits = ["Apple", "Banana"];
 fruits.push("Mango");
 
 console.log(fruits);
+
+
+// Task 2
+// const numbers = [];
+// Using push(), add:
+// 10
+// 20
+// 30
+// 40
+// Expected:
+// [10, 20, 30, 40]
+
+const numbers = [];
+
+numbers.push(10);
+numbers.push(20);
+numbers.push(30);
+numbers.push(40);
+
+console.log(numbers);
