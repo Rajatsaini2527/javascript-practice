5b72ace63d12d865a5f4bd0761a52e4b5b72902e
Rajatsainiji <rajatsaini798317@gmail.com>
2026-07-13 22:07:09 +0530

    find the sum of all elements

---
+++ ArraysQuestion/arraysBasics.js
@@ -0,0 +1,11 @@
+// 1. Find the sum of all elements in an array
+function sumOfElements(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+const arr = [1, 2, 3, 4, 5];
+console.log(sumOfElements(arr)); 
