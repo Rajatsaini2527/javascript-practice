cffe0d955198a7b747c9232d8f72e345b46d2511
Rajatsainiji <rajatsaini798317@gmail.com>
2026-07-14 21:36:14 +0530

    find the sum of all elements

--- ArraysQuestion/arraysBasics.js
+++ ArraysQuestion/arraysBasics.js
@@ -1,11 +1,88 @@
-// 1. Find the sum of all elements in an array
-function sumOfElements(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
+// Find the largest element in an array.
+ 
+const arr = [4,2,6,7,9,6,4,9,5,2,];
+const findLargestNumber = (arr) => {
+    let largest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
     }
-    return sum;
+     console.log(largest)
+};
+findLargestNumber(arr)
+ 
+// Find the smallest element in an array.
+ 
+const findSmallestNumber = (arr) => {
+  let smallest = arr[0];
+  for(let i = 1; i < arr.length; i++ ){
+    if(arr[i] < smallest){
+      smallest = arr[i];
+    }
+  }
+  console.log(smallest);
 }
+ 
+findSmallestNumber(arr);
 
-const arr = [1, 2, 3, 4, 5];
-console.log(sumOfElements(arr)); 
+
+// Calculate the sum of all elements in an array.
+ 
+const sumOfAllElements = (arr) => {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  console.log(sum);
+}
+ 
+sumOfAllElements(arr);
+ 
+// Find the average of array elements.
+ 
+const findAverageNumber = (arr) => {
+  let sum = 0;
+  let averag = 0
+ 
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+ 
+  averag = sum % arr.length;
+ 
+  console.log(averag);
+}
+ 
+findAverageNumber(arr);
+ 
+// Count even and odd numbers in an array.
+ 
+const countEvenAndOdd = (arr) => {
+  let odd = 0;
+  let even = 0
+ 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even  ++;
+    }
+ 
+    if (arr[i] % 2 !== 0) {
+      odd  ++;
+    }
+  }
+  console.log(`Even numbers: ${even}, Odd numbers: ${odd}`);
+}
+ 
+const arr = [4,2,6,7,9,6,4,9,5,2,];
+countEvenAndOdd(arr)
+ 
+// Reverse an array.
+ 
+const arrayReverse = (arr) => {
+  for(let i = arr.length -1; i >= 0; i--){
+    console.log(arr[i]);
+  }
+}
+ 
+arrayReverse(arr);
